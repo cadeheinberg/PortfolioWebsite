78ca5cf41b41f7087829a87df81f4ef45da4a54c
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2024-04-18 16:34:40 -0500

    javascript read more btns

--- projects.js
+++ projects.js
@@ -1,15 +1,37 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+if (document.readyState == 'loading') {
+    //if it is still loading
+    //we add an event listener so that as soon
+    //as it is loaded ("DOMContentLoaded")
+    //we run the function "ready"
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    //if the document is already loaded
+    //then the html is good to go we are ready
+    ready()
+}
 
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Read more";
-        moreText.style.display = "none";
+function ready() {
+    var readMoreButtons = document.getElementsByClassName('post-read-more-svg')
+    for (var i = 0; i < readMoreButtons.length; i++) {
+        var button = readMoreButtons[i]
+        button.addEventListener('click', readMoreClick)
+    }
+}
+
+function readMoreClick(event) {
+    // when you use target is gets the actual thing clicked
+    // when you use currentTarget you get the element which has attached the event handler
+    var svg_btn = event.currentTarget;
+    var post = svg_btn.parentElement;
+    var more_text = post.getElementsByClassName('post-content-hide')[0];
+    if (more_text.style.maxHeight && more_text.style.maxHeight !== "0px") {
+        // hide read more
+        more_text.style.maxHeight = "0px";
+        svg_btn.setAttribute("transform", "rotate(0)");
     } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "Read less";
-        moreText.style.display = "inline";
+        // expand read more
+        // Set a max-height that's greater than the height of the content
+        more_text.style.maxHeight = more_text.scrollHeight + "px";
+        svg_btn.setAttribute("transform", "rotate(180)");
     }
 }
